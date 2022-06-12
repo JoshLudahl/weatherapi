30b11190df2ec5fd30e46a4e4b966feaed960b96
joshy <josh.ludahl@gmail.com>
2022-06-12 15:55:10 -0700

    AQI

---
+++ src/main/java/com/softklass/weatherapi/AirQuality.java
@@ -0,0 +1,11 @@
+package com.softklass.weatherapi;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AirQuality {
+
+    public String getAirQuality() {
+        return "";
+    }
+}
