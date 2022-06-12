484f792df3a79c3d424be2ee427ec8c54ab40ede
joshy <josh.ludahl@gmail.com>
2022-06-12 16:27:03 -0700

    orgs

--- src/main/java/com/softklass/weatherapi/controllers/CitySuggestion.java
+++ src/main/java/com/softklass/weatherapi/controllers/CitySuggestion.java
@@ -1,4 +1,4 @@
-package com.softklass.weatherapi;
+package com.softklass.weatherapi.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
