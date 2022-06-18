2f1e0b3d69e35d0184c54a2eada56eadf0c307c2
joshy <josh.ludahl@gmail.com>
2022-06-18 15:55:57 -0700

    removed imports

--- src/main/java/com/softklass/weatherapi/network/WeatherService.java
+++ src/main/java/com/softklass/weatherapi/network/WeatherService.java
@@ -3,8 +3,6 @@ package com.softklass.weatherapi.network;
 import com.softklass.weatherapi.model.weatherrequest.WeatherRequest;
 import com.softklass.weatherapi.model.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
